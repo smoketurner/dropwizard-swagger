abd78620b234090ed567068bccba068409d3c89e
John M Flinchbaugh <jflinchbaugh@sonatype.com>
2023-05-19 10:54:13 -0400

    upgrade to swagger-jersey2-jaxrs to 1.6.9 (#7)
    gets us to reflections 0.10.2 which has a clearer license

--- src/main/java/io/federecio/dropwizard/swagger/FastBeanConfig.java
+++ src/main/java/io/federecio/dropwizard/swagger/FastBeanConfig.java
@@ -59,9 +59,11 @@ public class FastBeanConfig extends BeanConfig {
      * set input filter: speed up scanning and remove warnings on fat jars
      */
     if (!allowAllPackages) {
-      config.setInputsFilter(
-          new FilterBuilder()
-              .includePackage(acceptablePackages.toArray(new String[acceptablePackages.size()])));
+      final FilterBuilder fb = new FilterBuilder();
+      for (final String p : acceptablePackages) {
+        fb.includePackage(p);
+      }
+      config.setInputsFilter(fb);
     }
 
     final Reflections reflections = new Reflections(config);
